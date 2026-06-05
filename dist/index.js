bb91204c44bee1c3bdf275eb3f46de2449e0ea56
bernardlabossiere <bernard.labossiere@outlook.com>
2026-06-05 11:08:48 -0400

    Remove dist from repo - built automatically via prepare script

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-import { GreeACPlatform } from './platform.js';
-import { PLATFORM_NAME } from './settings.js';
-/**
- * This method registers the platform with Homebridge
- */
-export default (api) => {
-    api.registerPlatform(PLATFORM_NAME, GreeACPlatform);
-};
-//# sourceMappingURL=index.js.map
